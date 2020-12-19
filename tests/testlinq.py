b015c7f30447b25cd5aef67adca70fd571a1aef8
Sergey Popov <sergei.popov174@gmail.com>
2020-12-20 02:22:10 +0500

    add select, where, orderby, tests

--- tests/testlinq.py
+++ tests/testlinq.py
@@ -16,11 +16,19 @@ class TestLinq(unittest.TestCase):
                             People("Sergey", "OrkJump", 17),
                             People("OddHub", "Doter", 33)]
 
-    def test_select_gt_age25_two_people(self):
-        people = Linq(self.test_people).select(lambda x: x.age > 25)
-        print(type(people))
+    def test_where_gt_age25_two_people(self):
+        people = Linq(self.test_people).where(lambda x: x.age > 25)
         self.assertEqual(len(people), 2)
 
-    def test_select_name_sergey_two_people(self):
-        people = Linq(self.test_people).select(lambda x: x.firstname == "Sergey")
+    def test_where_name_sergey_two_people(self):
+        people = Linq(self.test_people).where(lambda x: x.firstname == "Sergey")
         self.assertEqual(len(people), 2)
+
+    def test_order_by_age(self):
+        expect = list(sorted(self.test_people, key=lambda x: x.age))
+        real = Linq(self.test_people).order_by(lambda x: x.age).to_list()
+        self.assertEqual(real, expect)
+
+    def test_select_age(self):
+        age_list = Linq(self.test_people).select(lambda x: x.age).to_list()
+        self.assertEqual(age_list, [23, 21, 45, 17, 33])
--- uglyfunc/linq.py
+++ uglyfunc/linq.py
@@ -3,10 +3,20 @@ from typing import Callable
 
 class Linq:
     def __init__(self, data):
-        self.data = data
+        self.data = [d for d in data]
 
     def select(self, condition: Callable):
+        return Linq([condition(item) for item in self.data])
+
+    def where(self, condition: Callable):
         return Linq(list(filter(condition, self.data)))
+
+    def order_by(self, condition: Callable):
+        return Linq(list(sorted(self.data, key=condition)))
 
     def __len__(self):
         return len(self.data)
+
+    def to_list(self):
+        return self.data
+
